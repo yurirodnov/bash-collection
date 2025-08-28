30d6a760941a3022d24b575a2e0d2627fb92c15f
Yuri <somewaystodo@yandex.ru>
2025-08-28 20:27:58 +0300

    add template and generator file

--- 000-basics/for.sh
+++ 000-basics/for.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
-
+# ====================================
+# Name: for.sh
+# Description: demonstration of 'for' construction basics
+# Author: Yuri Rodnov
+# ====================================
 
 
 #for i in 1 2 3 4 5; do
 #	echo $(( $i * 2))
 #done
 
-
 ITEMS=/home/yuri/*
-
-
 
 for i in $ITEMS; do
 	if [[ -d $i ]]; then
