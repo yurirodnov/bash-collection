2016c7b6d86844d38ceffe58a52cfb9884531fb8
Yuri <somewaystodo@yandex.ru>
2025-08-30 12:58:42 +0300

    change generator code

--- script_generator.sh
+++ script_generator.sh
@@ -4,27 +4,28 @@
 EXTENSION=".sh"
 TEMPLATE="template.sh"
 
-
-
 read -p "New script name: " NEW_SCRIPT_NAME
 read -p "New script directory: " NEW_SCRIPT_DIR
+
+read -p "New script description: " DESCRIPTION
+DESCRIPTION=${DESCRIPTION:-"-"}
 
 if [[ "$NEW_SCRIPT_NAME" != *"$EXTENSION" ]]; then
 	NEW_SCRIPT_NAME="${NEW_SCRIPT_NAME}${EXTENSION}"
 fi
 
-
 NEW_SCRIPT="$NEW_SCRIPT_DIR/$NEW_SCRIPT_NAME"
-
-read -p "Введите описание скрипта (опционально): " DESCRIPTION
-DESCRIPTION=${DESCRIPTION:-"Описание отсутствует"}
-
 
 sed -e "s/Name: -/Name: $NEW_SCRIPT_NAME/" \
     -e "s/Description: -/Description: $DESCRIPTION/" \
     "$TEMPLATE" > "$NEW_SCRIPT"
 
-# Делаем файл исполняемым
 chmod +x "$NEW_SCRIPT"
 
+
+if [[ -f"$NEW_SCRIPT" ]]; then
+	echo "Script $NEW_SCRIPT has created"
+else
+	echo "Something went wrong"
+fi
 
