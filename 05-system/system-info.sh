5dc80d44f4bb853320245e52c06415b1a60d6db2
Yuri <somewaystodo@yandex.ru>
2025-09-16 12:13:55 +0300

    add package manager

--- 05-system/system-info.sh
+++ 05-system/system-info.sh
@@ -10,15 +10,28 @@
 
 LINES="-------------------------------------"
 MAIN_INTERFACE=$(ip route | grep default | awk '{print $5}')
+PCKG_MGR=""
+
+for pm in apt dnf yum pacman zypper apk dpkg rpm; do
+	if command -v "$pm" &>/dev/null; then
+		if [[ -z "$PCKG_MGR" ]]; then
+			PCKG_MGR="$pm"
+		else
+			PCKG_MGR="$PCKG_MGR, $pm"
+		fi
+	fi
+done
 
 
 #echo $LINES
+echo
 echo "System"
 echo $LINES
 
 echo "Hostname: $(hostname)"
 echo "Operational system: $(grep PRETTY_NAME /etc/os-release | cut -d= -f2)"
 echo "Distributive code name: $(grep VERSION_CODENAME /etc/os-release | cut -d= -f2)"
+echo "Package manager: $PCKG_MGR"
 echo "Kernel: $(uname -r)"
 echo "Architecture: $(uname -m)"
 echo "Virtualization: $(systemd-detect-virt)"
@@ -53,8 +66,10 @@ echo "Users logged-in: $(who)"
 
 echo
 #echo $LINES
-echo "Another info"
+echo "Misc"
 echo $LINES
 
-echo "Timezone: $(timedatectl | grep 'Time zone:' | awk '{print $3}')"
+echo "Shell: $("$SHELL" --version | head -1)"
+echo "Timezone: $(timedatectl | grep 'Time zone:' | awk -F: '{print $2}')"
+echo
 
